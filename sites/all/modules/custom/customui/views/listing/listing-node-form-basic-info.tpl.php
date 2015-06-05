39bf0ef81be50ede3114bb61290f282e893235fb
John Kyeyune <genesint@gmail.com>
2015-06-05 17:01:50 +0100

    Replaced custom basic info form

--- sites/all/modules/custom/customui/views/listing/listing-node-form-basic-info.tpl.php
+++ sites/all/modules/custom/customui/views/listing/listing-node-form-basic-info.tpl.php
@@ -13,37 +13,13 @@ $form = drupal_get_form('listing' . '_node_form', $node);
 #$form["field_listing_location"]['#access'] = 0;
 $form["body"]['#access'] = 0;
 $form['actions']["preview"]['#access'] = 0;
+
 ?>
 <div class="row">
     <div class="col-md-2"></div>
-    <div class="col-md-4">
-        <?php print render($form['title']); ?>
-    </div>
-    <div class="col-md-6"></div>
-</div>
-
-<div class="row">
-    <div class="col-md-2"></div>
-    <div class="col-md-4">
-        <?php print render($form['field_address']); ?>
-    </div>
-    <div class="col-md-6"></div>
-</div>
-
-<div class="row">
-    <div class="col-md-2"></div>
     <div class="col-md-6">
-        <?php print render($form['field_listing_location']); ?>
+        <?php print drupal_render($form); ?>
     </div>
     <div class="col-md-4"></div>
-</div>
-
-<div class="row">
-    <div class="col-md-2">
-    </div>
-    <div class="col-md-8">
-        <?php print drupal_render_children($form); ?>
-    </div>
-    <div class="col-md-2"></div>
 </div>
 
